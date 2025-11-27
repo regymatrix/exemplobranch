79c676e6abc181e8e56b9b0f005c4e374e1e2aea
regymatrix <105677816+regymatrix@users.noreply.github.com>
2025-11-27 14:11:06 -0300

    imc programa

--- senaiprojetosemsala/imcprojeto/Program.cs
+++ senaiprojetosemsala/imcprojeto/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("PROGAMA CALCULAR MÉDIA");
+Console.WriteLine("Digite a primeira nota");
+decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a segunda nota");
+decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a terceira nota");
+decimal nota3 = Convert.ToDecimal(Console.ReadLine());
+
+decimal media = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine($"A média é: {media} ");
+
+if (media >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
